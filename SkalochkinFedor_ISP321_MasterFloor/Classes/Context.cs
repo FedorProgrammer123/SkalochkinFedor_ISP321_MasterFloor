6898da6d852c2c155914578a84b2be0e2242a9e0
FrontendNinja <skalockinfedor57@gmail.com>
2025-04-26 19:04:59 +0300

    AddListView

---
+++ SkalochkinFedor_ISP321_MasterFloor/Classes/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SkalochkinFedor_ISP321_MasterFloor.DataBase;
+namespace SkalochkinFedor_ISP321_MasterFloor.Classes
+{
+    public class Context:DbContext
+    {
+        public static Context _context;
+        public DbSet<PartnersImport> partnersImport { get; set; }
+        public static Context GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new Context();
+            }
+            return _context;
+        }
+        public Context() : base("name=SkalochkinFedor_ISP321_MasterFloorEntities")
+        {
+
+        }
+    }
+}
---
+++ SkalochkinFedor_ISP321_MasterFloor/Pages/PrimaryPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SkalochkinFedor_ISP321_MasterFloor.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkalochkinFedor_ISP321_MasterFloor.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PrimaryPage.xaml
+    /// </summary>
+    public partial class PrimaryPage : Page
+    {
+        public PrimaryPage()
+        {
+            InitializeComponent();
+            ShowPartners.ItemsSource = Classes.Context.GetContext().partnersImport.ToList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.AddUser());
+        }
+    }
+}
